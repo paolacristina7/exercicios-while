ebb9a7c1c6b783632bef64c44982b33c53d550e3
paolacristina7 <elacristina1988@gmail.com>
2026-05-13 21:22:58 -0300

    feat (EX06):calculadora

--- untitled/src/calculadorasimples.java
+++ untitled/src/calculadorasimples.java
@@ -3,41 +3,40 @@ import java.util.Scanner;
 public class calculadorasimples {
     static void main() {
         Scanner sc = new Scanner(System.in);
-        IO.println("Digite um numero");
+        IO.println("Digite um number");
         int numero1= sc.nextInt();
-        IO.println("Digite outro numero");
-        int numero2= sc.nextInt();
+        IO.println("Digite outro number");
         int opcão=0;
-        int conta=0;
+        int numero2= sc.nextInt();
+        int calculo =0;
         do {
-            IO.println("Escolha uma opção para continuar");
-            IO.println("Somar(1)");
-            IO.println("Subtrair(2)");
-            IO.println("Multiplicar(3)");
-            IO.println("Dividir(4)");
-            IO.println("Sair(5)");
+            IO.println("veja uma opção para continuar");
+            IO.println("somar(1)");
+            IO.println("subtrair(2)");
+            IO.println("multiplicar(3)");
+            IO.println("dividir(4)");
+            IO.println("sair(5)");
             opcão= sc.nextInt();
             if (opcão==1){
-                conta= numero2+numero1;
-                IO.println("A soma é igual a :"+conta);
+                calculo = numero2+numero1;
+                IO.println("A soma é igual a :"+ calculo);
 
             } else if (opcão==2) {
-                conta= numero2-numero1;
-                IO.println("A subtração é igual a:"+conta);
+                calculo = numero2-numero1;
+                IO.println("A subtração é igual a:"+ calculo);
 
             } else if (opcão==3) {
-                conta= numero2*numero1;
-                IO.println("A multiplicação é igual a:"+conta);
+                calculo = numero2*numero1;
+                IO.println("A multiplicação é igual a:"+ calculo);
 
             }else if (opcão==4){
-                conta= numero2/numero1;
-                IO.println("A divisão é igual a:"+conta);
+                calculo = numero2/numero1;
+                IO.println("A divisão é igual a:"+ calculo);
 
             } else if (opcão==5) {
             }else {
-                IO.println("Opção inválida");
+                IO.println("alternativa incorreta");
             }
         }while (opcão!=5);
 
-    }
-}
+    }}
